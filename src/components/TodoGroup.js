a46a7410817c100de737926092f499365e0d190c
Jens Jovellano <jensjovellano@gmail.com>
2023-08-28 21:21:03 +0800

    Refactor: Changed header from List of Todo -> To-Do List:

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -2,7 +2,7 @@ import TodoItem from "./TodoItem";
 const TodoGroup = (todoList) => {
     return (
         <div>
-            <h1>List of Todo</h1>
+            <h1>To-Do List:</h1>
             <TodoItem todoList={todoList} />
         </div>
     )
