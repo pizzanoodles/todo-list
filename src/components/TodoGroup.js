ad3ec7c6cccd71bc144bd9afbb0c37a7587f0ac8
Jens Jovellano <jensjovellano@gmail.com>
2023-08-29 18:57:38 +0800

    Feat: Added confirm for delete using react-confirm-alert

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -1,9 +1,26 @@
 import TodoItem from "./TodoItem";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteTodo, toggleDone } from "./TodoSlice";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
 const TodoGroup = () => {
     const dispatch = useDispatch();
     const selector = useSelector((state) => state.todo.todoList);
+    const confirmDelete = (index) => {
+        confirmAlert({
+            title: "Confirm delete of task",
+            message: "Are you sure you want to delete this task?",
+            buttons: [
+                {
+                    label: "Yes",
+                    onClick: () => onDeleteHandler(index)
+                },
+                {
+                    label: "No"
+                }
+            ]
+        })
+    }
     const onDeleteHandler = (index) => {
         dispatch(deleteTodo(index))
     }
@@ -15,8 +32,8 @@ const TodoGroup = () => {
             <h1>To-Do List:</h1>
             <table className="todoTable">
                 {selector.map((todoItem, index) => <tr key={index} className="todoItem">
-                    <td className={todoItem.done ? "done" : ""} onClick={() => onToggleHandler(index)}><TodoItem todoItem={todoItem} /></td>
-                    <td><button className="deleteBtn" onClick={() => onDeleteHandler(index)}>X</button></td>
+                    <td className={todoItem.done ? "tdDoneTodo" : "tdNotDoneTodo"} onClick={() => onToggleHandler(index)}><TodoItem todoItem={todoItem} /></td>
+                    <td className="tdDeleteBtn"><button className="deleteBtn" onClick={() => confirmDelete(index)}>X</button></td>
                 </tr>)}
             </table>
         </div>
