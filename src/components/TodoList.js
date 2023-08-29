30c4a5a64dca32fc29cdc0270ed5b71a5bb95338
Jens Jovellano <jensjovellano@gmail.com>
2023-08-29 18:20:39 +0800

    Refactor: Removed unused imports and variables

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,13 @@
 import TodoGroup from "./TodoGroup";
 import TodoGenerator from "./TodoGenerator";
-import { useState } from "react";
-import { useSelector } from "react-redux";
 const TodoList = () => {
-    const [todoList, setTodoList] = useState([]);
-    const selector = useSelector((state) => state.todo.todoList);
-    console.log(selector);
     return (
         <div className="todoList">
             <div className="todoGroup">
-                <TodoGroup todoList={selector}/>
+                <TodoGroup />
             </div>
             <div className="todoGenerator">
-                <TodoGenerator todoList={todoList} setTodoList={setTodoList} />
+                <TodoGenerator />
             </div>
         </div>
     )
