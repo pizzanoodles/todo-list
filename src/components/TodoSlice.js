f33c42c12151f81dcf9d4f008b9b2213e43903b3
Jens Jovellano <jensjovellano@gmail.com>
2023-08-29 18:18:35 +0800

    Feat: Added deleteTodo and toggleDone reducer for TodoSlice

--- src/components/TodoSlice.js
+++ src/components/TodoSlice.js
@@ -7,8 +7,14 @@ const todoSlice = createSlice({
     reducers: {
         addTodo: (state, action) => {
             state.todoList.push(action.payload);
+        },
+        deleteTodo: (state, action) => {
+            state.todoList.splice(action.payload, 1);
+        },
+        toggleDone: (state, action) => {
+            state.todoList[action.payload].done = !state.todoList[action.payload].done;
         }
     }
 });
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, deleteTodo, toggleDone } = todoSlice.actions;
 export default todoSlice.reducer;
