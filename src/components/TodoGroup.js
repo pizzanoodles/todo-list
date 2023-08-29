671e94e64cfcf008cab70e6f2a8cd916d2d40d91
Jens Jovellano <jensjovellano@gmail.com>
2023-08-29 18:18:15 +0800

    Feat: Modified TodoGroup to utilize Redux

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -1,10 +1,23 @@
 import TodoItem from "./TodoItem";
-const TodoGroup = ({todoList}) => {
+import { useDispatch, useSelector } from "react-redux";
+import { deleteTodo, toggleDone } from "./TodoSlice";
+const TodoGroup = () => {
+    const dispatch = useDispatch();
+    const selector = useSelector((state) => state.todo.todoList);
+    const onDeleteHandler = (index) => {
+        dispatch(deleteTodo(index))
+    }
+    const onToggleHandler = (index) => {
+        dispatch(toggleDone(index))
+    }
     return (
         <div>
             <h1>To-Do List:</h1>
             <table className="todoTable">
-                {todoList.map((todoItem) => <tr className="todoItem"><td><TodoItem todoItem={todoItem} /></td></tr>)}
+                {selector.map((todoItem, index) => <tr key={index} className="todoItem">
+                    <td className={todoItem.done ? "done" : ""} onClick={() => onToggleHandler(index)}><TodoItem todoItem={todoItem} />
+                        <button onClick={() => onDeleteHandler(index)}>X</button></td>
+                </tr>)}
             </table>
         </div>
     )
