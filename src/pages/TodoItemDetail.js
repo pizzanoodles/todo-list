afc756dcf3354e2829b3a538c0e7feb7e17d9401
Jens Jovellano <jensjovellano@gmail.com>
2023-08-31 01:15:20 +0800

    Feat: Added text and design for user experience also added a back button from AntD using FloatButton with custom Icon

--- src/pages/TodoItemDetail.js
+++ src/pages/TodoItemDetail.js
@@ -1,13 +1,29 @@
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import "../css/todoItemDetail/todoItemDetail.css";
+import { FloatButton } from "antd";
+import { RollbackOutlined } from "@ant-design/icons";
 const TodoItemDetail = () => {
+    const navigate = useNavigate();
     const { id } = useParams();
     const todoItem = useSelector((state) => state.todo.todoList.find((todoItem) => todoItem.id === id));
+    const goBackPage = () => {
+        navigate(-1);
+    }
     return (
-        <div>
-            <h1>Todo Item Detail</h1>
-            <div>{todoItem.id}</div>
-            <div>{todoItem.text}</div>
+        <div className="todoItemDetail">
+            <FloatButton
+            onClick={() => goBackPage()}
+                icon={<RollbackOutlined />} 
+                type="primary"
+                style={{
+                    left:490,
+                    top:210
+                }}
+                />
+            <h1>Todo Item Details:</h1>
+            <p>Task ID: {todoItem.id}</p>
+            <p>Task Description: {todoItem.text}</p>
         </div>
     )
 };
