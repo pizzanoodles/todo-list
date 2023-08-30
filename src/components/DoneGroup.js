90ebb6ef6c8a844af2601855763c1a841fc2340f
Jens Jovellano <jensjovellano@gmail.com>
2023-08-31 01:13:08 +0800

    Feat: Utilized AntD to display Done Items using List

--- src/components/DoneGroup.js
+++ src/components/DoneGroup.js
@@ -1,11 +1,21 @@
-import DoneItem from "./DoneItem";
+import { useNavigate } from "react-router-dom";
+import "../css/doneGroup/doneGroup.css";
+import { Space, Row, List, Typography } from "antd";
 const DoneGroup = (props) => {
+    const navigate = useNavigate();
     const todoItems = props.todoItems.filter((todoItem) => todoItem.done);
-    return todoItems.map((todoItem) => {
-        return (
-            <DoneItem todoItem={todoItem}
-            key={todoItem.id} />
-        )
-    })
+    const onClickTodoItem = (id) => {
+        navigate("/done/" + id);
+    };
+    return (
+        <List size="large"
+            pagination = {{pageSize: 5}}
+            bordered
+            dataSource={todoItems}
+            renderItem={(item) => (
+                <List.Item key="Id" onClick={() => onClickTodoItem(item.id)}>{item.text}</List.Item>
+            )}
+        />
+    )
 }
 export default DoneGroup;
