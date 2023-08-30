f225c954cb0f649663a52920175789dd06480c0d
Jens Jovellano <jensjovellano@gmail.com>
2023-08-31 01:14:52 +0800

    Feat: Added text and design for user experience

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,10 +1,13 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, NavLink } from "react-router-dom";
+import "../css/errorPage/errorPage.css";
 const ErrorPage = () => {
     const error = useRouteError();
     return (
-        <div>
-            <h2>{error.statusText}</h2>
-            <h2>{error.message}</h2>
+        <div className="errorPage">
+            <h1>Are you lost?</h1>
+            <p>The url you entered is: {error.statusText}!</p>
+            <p>{error.error.message}</p>
+            <p>If you would like to go back to the home page, click <NavLink to="/">here.</NavLink> </p>
         </div>
     )
 }
