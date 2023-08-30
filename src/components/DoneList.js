403803db5f87e93a8dd076519e830fee442f8541
Jens Jovellano <jensjovellano@gmail.com>
2023-08-31 01:13:26 +0800

    Feat: Added classname to div for design purposes

--- src/components/DoneList.js
+++ src/components/DoneList.js
@@ -1,10 +1,10 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import DoneGroup from "./DoneGroup";
 const DoneList = () => {
     const tasks = useSelector((state) => state.todo.todoList);
     return (
-        <div>
-            <h1>Done Items</h1>
+        <div className="doneGroup">
+            <h1 className="doneItemsHeader">Done Items:</h1>
             <DoneGroup todoItems={tasks} />
         </div>
     )
